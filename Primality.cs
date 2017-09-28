b2c2fc98ba7208452d71fcec5c3231eb271c2ab9
rushaniia.badrutdinova <rushaniia.badrutdinova@gmail.com>
2017-09-28 16:51:16 +0300

    Add files via upload

---
+++ Primality.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Primality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < p; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (IsPrime(n))
+                    Console.WriteLine("Prime");
+                else Console.WriteLine("Not prime");
+            }
+        }
+        public static bool IsPrime(int a)
+        {
+            if (a == 2)
+                return true;
+            int sqrt = (int)Math.Sqrt(a);
+            for (int i = 2; i < sqrt + 1; i++)
+            {
+                if (a % i == 0 || a == 1)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
